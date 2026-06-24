fb403574c9aa08bd67037453a5e9cb935d9d4d5a
Tanbeer <tanbeerhasan7@gmail.com>
2026-06-24 17:07:36 +0600

    :boom: update server js file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Comment out the line below if you want to deploy as a dynamic Node.js server using server.js
   output: "export",
   images: {
     unoptimized: true,
